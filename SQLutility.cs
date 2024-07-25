e80a75b57eec6fdada5dac64498b12816a56c443
RassiLevine <rassilevine@gmail.com>
2024-07-25 09:58:56 -0400

    #45

--- SQLutility.cs
+++ SQLutility.cs
@@ -1,33 +1,67 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace CPUFramework
 {
     public class SQLutility
     {
         public static string ConnectionString = "";
+
+        public static SqlCommand GetSqlCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLutility.ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("-----" + Environment.NewLine +cmd.CommandText);
+            DataTable dt = new();
+            using(SqlConnection conn = new SqlConnection(SQLutility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
+            AllowColumnNull(dt);
+            return dt;
+        }
         public static DataTable GetDataTable(string sqlstatement)
         {
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-            //DisplayMessage("connection status", conn.State.ToString());
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-            AllowColumnNull(dt);
-
-            return dt;
+           
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
         public static void ExecuteSQL(string sqlstatement)
         {
             GetDataTable(sqlstatement);
         }
         
+        public static int GetFirstRowFirstColumn(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value) 
+                { 
+
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+            }
+            return n;
+        }
         private static void AllowColumnNull(DataTable dt)
         {
             foreach(DataColumn c in dt.Columns)
