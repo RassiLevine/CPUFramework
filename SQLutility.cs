757f08ef8ae34567947e5db4335139835d6e0297
RassiLevine <rassilevine@gmail.com>
2024-07-31 12:02:50 -0400

    #20

--- SQLutility.cs
+++ SQLutility.cs
@@ -1,12 +1,10 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
 using System.Diagnostics;
-using System.Net.NetworkInformation;
 using System.Text;
 
 namespace CPUFramework
-{ 
+{
     public class SQLutility
     {
         public static string ConnectionString = "";
@@ -32,8 +30,16 @@ namespace CPUFramework
             {
                 conn.Open();
                 cmd.Connection = conn;
-                SqlDataReader dr = cmd.ExecuteReader();
-                dt.Load(dr);
+                try
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                }
+                catch (SqlException ex)
+                {
+                    string msg = ParseConstraintMessage(ex.Message);
+                    throw new Exception(msg);
+                }
             }
             AllowColumnNull(dt);
             return dt;
@@ -70,77 +76,12 @@ namespace CPUFramework
                 c.AllowDBNull = true;
             }
         }
-     //public static string ConnectionString = "";
-
-     //       public static SqlCommand GetSqlCommand(string sprocname)
-     //       {
-     //           SqlCommand cmd;
-     //           using (SqlConnection conn = new SqlConnection(SQLutility.ConnectionString))
-     //           {
-     //               cmd = new SqlCommand(sprocname, conn);
-     //               cmd.CommandType = CommandType.StoredProcedure;
-     //               conn.Open();
-     //               SqlCommandBuilder.DeriveParameters(cmd);
-     //           }
-     //           return cmd;
-     //       }
-
-     //       public static DataTable GetDataTable(SqlCommand cmd)
-     //       {
-     //          //ebug.Print("-----" + Environment.NewLine + cmd.CommandText);
-     //           DataTable dt = new();
-     //           using (SqlConnection conn = new SqlConnection(SQLutility.ConnectionString))
-     //           {
-     //               conn.Open();
-     //               cmd.Connection = conn;
-     //           Debug.Print(GetSql(cmd));
-     //           SqlDataReader dr = cmd.ExecuteReader();
-     //               dt.Load(dr);
-     //           }
-     //           AllowColumnNull(dt);
-     //           return dt;
-     //       }
-     //       public static DataTable GetDataTable(string sqlstatement)
-     //       {
-
-     //           return GetDataTable(new SqlCommand(sqlstatement));
-     //       }
-     //       public static void ExecuteSQL(string sqlstatement)
-     //       {
-     //           GetDataTable(sqlstatement);
-     //       }
-
-     //       public static int GetFirstRowFirstColumn(string sql)
-     //       {
-     //           int n = 0;
-
-     //           DataTable dt = GetDataTable(sql);
-     //           if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
-     //           {
-     //               if (dt.Rows[0][0] != DBNull.Value)
-     //               {
-
-     //                   int.TryParse(dt.Rows[0][0].ToString(), out n);
-     //               }
-     //           }
-     //           return n;
-     //       }
-     //       private static void AllowColumnNull(DataTable dt)
-     //       {
-     //           foreach (DataColumn c in dt.Columns)
-     //           {
-     //               c.AllowDBNull = true;
-     //           }
-     //       }
-
         public static string GetSql(SqlCommand cmd)
         {
             string val = "";
-#if Debug
+#if DEBUG
             StringBuilder sb = new StringBuilder();
-            int paramcount = cmd.Parameters.Count - 1;
-            int paramnum = 0;
-            string comma = ",";
+           
             if (cmd.Connection != null)
             {
                 sb.AppendLine($"--{cmd.Connection.DataSource}");
@@ -150,6 +91,9 @@ namespace CPUFramework
             if (cmd.CommandType == CommandType.StoredProcedure)
             {
                 sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                string comma = ",";
                 foreach (SqlParameter p in cmd.Parameters)
                 {
                     if (p.Direction != ParameterDirection.ReturnValue)
@@ -174,6 +118,42 @@ namespace CPUFramework
             return val;
         }
 
+        private static string ParseConstraintMessage(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = "";
+            if(msg.Contains(prefix) == false)
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                msg = msg.Replace("\"", "'");
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf("'");
+                if (pos == -1)
+                {
+                  msg = origmsg;
+                }
+                else
+                {
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg = msg + msgend;
+                }
+            }
+            return msg;
+        }
         public static void DebugPrintDataTable(DataTable dt)
             {
                 foreach (DataRow r in dt.Rows)
